80dff756510c72eec1331eedeaddedafeced0c9f
evelyntheodora <129470436+evelyntheodora@users.noreply.github.com>
2023-05-26 10:41:39 +0700

    Initial Commit

---
+++ Git_Collab_Exercise/Git_Collab_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_Collab_ExerciseApp.swift
+//  Git_Collab_Exercise
+//
+//  Created by Evelyn Theodora Siswanto on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Git_Collab_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
